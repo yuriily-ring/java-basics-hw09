92423a4513e3e4768b6e03f9c49c2f6879ffb839
Yermek Kadyrbayev <user@mail.com>
2019-06-18 17:14:42 +0300

    first commit

---
+++ src/ArrayUtil.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+/**
+ * ArrayUtil exercises.
+ */
+public class ArrayUtil {
+
+  /**
+   * Finds the common elements between two arrays (String values).
+   *
+   * @param array1 first array
+   * @param array2 second array
+   * @return String array with common elements
+   */
+  public static String[] findCommon(String[] array1, String[] array2) {
+    // TODO fill in code here
+    return new String[0];
+  }
+}
---
+++ src/ArraysTest.java
@@ -0,0 +1,31 @@
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ArraysTest {
+
+  /**
+   * Test data for common array elements.
+   *
+   * @return test data array
+   */
+  @DataProvider(name = "ArrayUtil")
+  public static Object[][] arrays() {
+    return new Object[][]{
+        {
+            new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+            new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+            new String[]{"JAVA", "SQL"}
+        }
+        // TODO add 2 more test data here
+    };
+  }
+
+  @Test(dataProvider = "ArrayUtil")
+  public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+
+    assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
+        "Common elements are not correct");
+  }
+}
