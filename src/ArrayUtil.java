601ea10e6d0fa46b07537844f5a98ea957bfc8cd
Yuriy Lytovchenko <yuri.litovchenko@gmail.com>
2021-01-14 15:11:44 +0200

    Homework 9

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * ArrayUtil exercises.
  */
@@ -12,6 +17,16 @@ public class ArrayUtil {
      */
     public static String[] findCommon(String[] array1, String[] array2) {
         // TODO fill in code here
-        return new String[0];
+        ArrayList<String> commonItems = new ArrayList<>();
+        Set<String> array1Items = new HashSet<>(Arrays.asList(array1));
+        Set<String> array2Items = new HashSet<>(Arrays.asList(array2));
+
+        for (String item : array1Items) {
+            if (array2Items.contains(item))
+                commonItems.add(item);
+        }
+        String[] returnString = new String[commonItems.size()];
+        returnString = commonItems.toArray(returnString);
+        return returnString;
     }
 }
